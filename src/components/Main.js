b5757c379dc87c9e66ae8081ae9076e16b29babd
Thomas Sivertsen <thomass@cerpus.com>
2019-11-22 13:39:44 +0100

    Don't display paragraph if no description is set

--- src/components/Main.js
+++ src/components/Main.js
@@ -46,7 +46,9 @@ function Main(props) {
                     className={"h5p-order-priority-surface-info"}
                     ref={resourceContainer}
                 >
-                    <p className={'h5p-order-priority-description'}>{description}</p>
+                    {description && (
+                        <p className={'h5p-order-priority-description'}>{description}</p>
+                    )}
                 </div>
                 <Surface />
             </div>
