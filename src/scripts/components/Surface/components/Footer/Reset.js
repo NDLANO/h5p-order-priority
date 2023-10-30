b50d1c2568e3ae693d0c8eb398b7d9acc7a3f2b6
Oliver Tacke <o.tacke@posteo.de>
2023-10-30 12:11:35 +0100

    Remove custom popover

--- src/scripts/components/Surface/components/Footer/Reset.js
+++ src/scripts/components/Surface/components/Footer/Reset.js
@@ -1,5 +1,4 @@
-import React, { Fragment, useState, useRef, useMemo, useEffect } from 'react';
-import Popover from '@components/Popover/Popover.js';
+import React, { Fragment, useState } from 'react';
 import { useOrderPriority } from '@context/OrderPriorityContext.js';
 import './Reset.scss';
 
@@ -8,10 +7,8 @@ import './Reset.scss';
  * @returns {object} JSX element.
  */
 const Reset = () => {
-  const [showPopover, setPopover] = useState(false);
-  const [previousFocusElement, setPreviousFocusElement] = useState(null);
+  const [isShowingDialog, setShowingDialog] = useState(false);
   const orderPriorityContext = useOrderPriority();
-  const resetButtonRef = useRef(null);
 
   const {
     behaviour: { enableRetry = false },
@@ -19,79 +16,42 @@ const Reset = () => {
     translations,
   } = orderPriorityContext;
 
-  useEffect(() => {
-    if (showPopover) {
-      setPreviousFocusElement(document.activeElement);
-    }
-  }, [showPopover]);
+  const showConfirmationDialog = () => {
+    setShowingDialog(true);
 
-  const togglePopover = (event) => {
-    if (!resetButtonRef.current) {
-      resetButtonRef.current = event?.target;
-    }
-    setPopover(!showPopover);
+    const dialog = new H5P.ConfirmationDialog({
+      headerText: translations.confirm,
+      dialogText: translations.ifYouContinueAllYourChangesWillBeLost,
+      cancelText: translations.cancel,
+      confirmText: translations.continue,
+      classes: ['restart']
+    });
+
+    dialog.on('confirmed', () => {
+      resetTask();
+      setShowingDialog(false);
+    });
+
+    dialog.on('canceled', () => {
+      setShowingDialog(false);
+    });
+
+    dialog.appendTo(document.body);
+    dialog.show();
   };
-
-  const confirmReset = () => {
-    resetTask();
-    togglePopover();
-  };
-
-  const openerRect = useMemo(
-    () => resetButtonRef.current?.getBoundingClientRect() ?? {},
-    []
-  );
 
   return (
     <Fragment>
       {enableRetry === true && (
-        <Popover
-          handleClose={togglePopover}
-          lastActiveElement={previousFocusElement}
-          show={showPopover}
-          classnames={orderPriorityContext.activeBreakpoints}
-          close={translations.close}
-          header={translations.restart}
-          align={'start'}
-          openerRect={openerRect}
-          popoverContent={(
-            <div
-              role={'dialog'}
-              aria-labelledby={'resetTitle'}
-              className={'h5p-order-priority-reset-modal'}
-            >
-              <p id={'resetTitle'}>
-                {translations.ifYouContinueAllYourChangesWillBeLost}
-              </p>
-              <div className="h5p-order-priority-reset-modal-buttons">
-                <button
-                  onClick={confirmReset}
-                  className={'popover-button continue'}
-                  type={'button'}
-                >
-                  {translations.continue}
-                </button>
-                <button
-                  onClick={togglePopover}
-                  className={'popover-button cancel'}
-                  type={'button'}
-                >
-                  {translations.cancel}
-                </button>
-              </div>
-            </div>
-          )}
+        <button
+          className={'h5p-order-priority-footer-button h5p-order-priority-button-restart'}
+          onClick={showConfirmationDialog}
+          aria-haspopup={'true'}
+          aria-expanded={isShowingDialog}
         >
-          <button
-            className={'h5p-order-priority-footer-button h5p-order-priority-button-restart'}
-            onClick={togglePopover}
-            aria-haspopup={'true'}
-            aria-expanded={showPopover}
-          >
-            <span className={'h5p-ri hri-restart'} />
-            {translations.restart}
-          </button>
-        </Popover>
+          <span className={'h5p-ri hri-restart'} />
+          {translations.restart}
+        </button>
       )}
     </Fragment>
   );
